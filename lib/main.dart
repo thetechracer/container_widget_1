1443c4cfa6ac6efb6dd23ccefbdb4367fa885979
thetechracer <olugbenganicholas2001@gmail.com>
2023-11-15 10:57:15 +0100

    Box shadow with container

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,35 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
+            alignment: const Alignment(0, 0),
             height: 300,
             width: 200,
             decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20),
-            ),
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(4.0, 4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0,
+                  ),
+                ]
+                //
+                // borderRadius: BorderRadius.circular(20),
+                ),
             // color: Colors.deepPurple,
+
+            child: const Text(
+              "Hola senorita",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
